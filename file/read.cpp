2999d40d5fc443a8b98e28590b1c1f0f5b09826e
lizhenbo <zhenbo.li@grandhonour.net>
2016-08-31 14:20:02 +0800

    new

---
+++ file/read.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+#define LEN 256
+int main()
+{
+	ifstream in;
+	in.open("./text", ios_base::in|ios_base::out);
+	if (!in.is_open())
+	{
+		cout << "open file error"<<endl;
+		return -1;
+	}
+	char buffer[LEN] = {0};
+	while (!in.eof())
+	{
+		in.getline(buffer, LEN);
+		cout << buffer;
+		cout << "\n";
+		//in >>  buffer;
+		//cout <<  " "<<buffer;
+	}
+	in.close();
+	return 0;
+}
