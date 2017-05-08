2acd15f5d0db0645665ab81a81198b15f50383fe
zenganiu <2086284702@qq.com>
2017-05-08 20:18:26 +0800

    add

--- init.cpp
+++ init.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int main()
 {
+
 /*	int values[]{1, 2, 3, 4};
 	vector<int> v{2, 3, 4, 5, 6, 7};
 	vector<string> cities{"Beijing", "Nanjing", "Shanghai"};
@@ -17,13 +18,16 @@ int main()
 	int *q{};
 	assert(q == nullptr);
 */
-	int x1(5.3);
+/*	int x1(5.3);
 	int x2 = 5.3;
 	int x3{5.0};
 	int x4 = {5.3};
 	char c1{7};
-	char c2{9999};
+	char c2{9999};*/
 	vector<int> v1{1, 2, 3, 4, 5,5};
 	vector<int> v2{1, 2.3, 3.4};
+	cout << "ve1's capacity "<<v1.capacity()<<endl;
+	cout << "ve1's size "<<v1.size()<<endl;
+
 	return 0;
 }
--- move.cpp
+++ move.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
+
 using namespace std;
+
 class example{
 private:
 	int n;
